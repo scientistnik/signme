f9892fc65efc5e56aa5b32295f3d4961e5b770eb
Nikolay Nozdrin-Plotnitsky <scientistnik89@gmail.com>
2020-07-18 23:14:58 +0300

    init commit

---
+++ src/baseAPI.ts
@@ -0,0 +1,77 @@
+import fetch from "cross-fetch";
+import formurlencoded from "form-urlencoded";
+
+type base64 = string;
+
+export interface FileConfig {
+  filet: base64;
+  fname: string;
+  md5?: string;
+}
+
+export interface SignFileConfig {
+  //key: string;
+  url?: string;
+  user_email?: string;
+  user_ph?: string;
+  user_snils?: string;
+  company_inn?: string;
+  company_ogrn?: string;
+  noemail?: number;
+  forcesms?: string;
+  IF?: number;
+}
+
+export interface SingleSignFileConfig extends FileConfig, SignFileConfig {
+  key: string;
+}
+
+export interface MultiSignFileConfig extends SignFileConfig {
+  key: string;
+  files: [FileConfig, ...FileConfig[]];
+}
+
+export interface SignConfig {
+  type: string;
+  id: string;
+  key: string;
+  user_ph: string;
+  passw: string;
+}
+
+export default class BaseAPI {
+  serverName: string;
+
+  constructor(serverName: string) {
+    this.serverName = serverName;
+  }
+
+  signFile(config: SingleSignFileConfig) {
+    return fetch(`${this.serverName}/signapi/sjson`, {
+      method: "POST",
+      body: JSON.stringify(config),
+    }).then((response) => response.text());
+  }
+
+  signFiles(config: MultiSignFileConfig) {
+    return fetch(`${this.serverName}/signapi/multijson`, {
+      method: "POST",
+      body: JSON.stringify(config),
+    }).then((response) => response.text());
+  }
+
+  signByUUID(config: SignConfig) {
+    return fetch(`${this.serverName}/signapi/sign`, {
+      method: "POST",
+      body: formurlencoded(config),
+      headers: { "Content-Type": "application/x-www-form-urlencoded" },
+    }).then((response) => response.text());
+  }
+
+  signatureCheck(md5: string) {
+    return fetch(`${this.serverName}/signaturecheck/json`, {
+      method: "POST",
+      body: JSON.stringify({ md5 }),
+    }).then((res) => res.json());
+  }
+}
---
+++ src/index.ts
@@ -0,0 +1,46 @@
+import BaseAPI, { SignFileConfig, FileConfig } from "./baseAPI";
+
+interface SignMeConfig {
+  apiKey: string;
+  serverName?: string;
+  userPhone?: string;
+  password?: string;
+}
+
+export default class SignMe {
+  baseAPI: BaseAPI;
+
+  private key: string;
+  private phone: string;
+  private password: string;
+
+  constructor(config: SignMeConfig) {
+    this.baseAPI = new BaseAPI(config.serverName || "https://sign.me");
+
+    this.key = config.apiKey;
+    this.phone = config.userPhone;
+    this.password = config.password;
+  }
+
+  sendFiles(files: [FileConfig, ...FileConfig[]], config?: SignFileConfig) {
+    return this.baseAPI.signFiles({
+      key: this.key,
+      files,
+      ...config,
+    });
+  }
+
+  sign(id: string) {
+    return this.baseAPI.signByUUID({
+      type: "multi",
+      id,
+      key: this.key,
+      user_ph: this.phone,
+      passw: this.password,
+    });
+  }
+
+  check(md5: string) {
+    return this.baseAPI.signatureCheck(md5);
+  }
+}
